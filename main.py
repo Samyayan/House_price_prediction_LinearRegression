1f156cf189142bada216f9d226e3bc282f0d963e
Samyayan <bitanrockz@gmail.com>
2025-07-06 21:49:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  6 20:48:07 2025
+
+@author: samya
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+loaded_model=pickle.load(open('trained_model.pkl', 'rb'))
+
+#Creating function for prediction
+def house_prediction(input_data):
+    
+    input_data_as_numpy=np.asarray(input_data)
+    input_data_as_reshaped=input_data_as_numpy.reshape(1,-1)
+    prediction=loaded_model.predict(input_data_as_reshaped)
+    return (prediction)
+    
+
+def main():
+    st.title ('House Price Prediction')
+    
+    avg_area_income=st.number_input('Enter avg. area income')
+    avg_area_house_age=st.number_input('Enter avg. area house age')
+    avg_area_number_of_rooms=st.number_input('Enter avg. area no of rooms')
+    avg_area_number_of_bedrooms=st.number_input('Enter avg. area no of bedrooms')
+    area_population=st.number_input('Enter area population')
+    
+    Price=''
+    if st.button('House price'):
+        Price=house_prediction([avg_area_income, avg_area_house_age, avg_area_number_of_rooms, avg_area_number_of_bedrooms, area_population])
+    
+    st.success(Price)
+
+
+
+if __name__=='__main__':
+    main()
